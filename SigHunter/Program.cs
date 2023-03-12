9c2a7894510a8acdb597ea6504242cddab52c7f1
Andrew Rathbun <36825567+AndrewRathbun@users.noreply.github.com>
2023-03-11 21:57:36 -0500

    update sigs

--- SigHunter/Program.cs
+++ SigHunter/Program.cs
@@ -10,18 +10,51 @@ class Program
     {
         // Define file signatures for each file type we're interested in
         Dictionary<string, string> signatures = new Dictionary<string, string>();
-        signatures.Add(".png", "89-50-4E-47-0D-0A-1A-0A");
-        signatures.Add(".pdf", "25-50-44-46-2D-31-2E");
-        signatures.Add(".mp4", "00-00-00-18-66-74-79-70");
-        signatures.Add(".sqlite", "53-51-4C-69-74-65-20-66"); // SQLite
+        signatures.Add(".3g2", "66-74-79-70-33-67-32"); // 3GPP2 multimedia file
+        signatures.Add(".3gp", "66-74-79-70-33"); // 3GPP multimedia file
+        signatures.Add(".7z", "37-7A-BC-AF-27-1C"); // 7-Zip archive
+        signatures.Add(".aac", "FF-F1"); // Advanced Audio Coding (AAC)
+        signatures.Add(".ac3", "0B-77"); // Audio Codec 3 (AC3)
+        signatures.Add(".aiff", "46-4F-52-4D"); // Audio Interchange File Format (AIFF)
+        signatures.Add(".amr", "23-21-41-4D-52"); // Adaptive Multi-Rate (AMR) audio
+        signatures.Add(".asf", "30-26-B2-75-8E-66-CF-11"); // Advanced Systems Format (ASF)
+        signatures.Add(".avi", "52-49-46-46"); // Audio Video Interleave (AVI)
+        signatures.Add(".bz2", "42-5A-68"); // BZip2 compressed file
+        signatures.Add(".class", "CA-FE-BA-BE"); // Java class file
+        signatures.Add(".dll", "4D-5A-50-00"); // Windows Dynamic Link Library
+        signatures.Add(".doc", "D0-CF-11-E0"); // Microsoft Word (97-2003)
         signatures.Add(".exe", "4D-5A"); // Windows executable
+        signatures.Add(".flac", "66-4C-61-43"); // Free Lossless Audio Codec (FLAC)
+        signatures.Add(".flv", "46-4C-56-01"); // Adobe Flash Video
+        signatures.Add(".gif", "47-49-46-38"); // Graphics Interchange Format (GIF)
+        signatures.Add(".html", "3C-21-44-4F"); // HTML document
         signatures.Add(".iso", "43-44-30-30-31"); // ISO-9660 CD/DVD image
-        signatures.Add(".zip", "50-4B-03-04"); // ZIP archive
-        signatures.Add(".jpg", "FF-D8-FF"); // JPEG image
-        signatures.Add(".jpeg", "FF-D8-FF"); // JPEG image
+        signatures.Add(".jpeg", "FF-D8-FF-E0"); // JPEG image
+        signatures.Add(".jpg", "FF-D8-FF-E0"); // JPEG image
+        signatures.Add(".lzw", "5A-57-53-31"); // Lempel-Ziv-Welch (LZW) compressed file
+        signatures.Add(".m4a", "00-00-00-20-66-74-79-70-4D-34-41-20"); // MPEG-4 audio
+        signatures.Add(".m4v", "00-00-00-20-66-74-79-70-4D-34-56-20"); // MPEG-4 video
+        signatures.Add(".mid", "4D-54-68-64"); // MIDI audio
+        signatures.Add(".midi", "4D-54-68-64"); // MIDI audio
+        signatures.Add(".mkv", "1A-45-DF-A3-93-42-82-88-6D-61-74-72-6F-73-6B-61"); // Matroska video
+        signatures.Add(".mov", "00-00-00-14-66-74-79-70-71-74-20-20"); // Apple QuickTime movie
         signatures.Add(".mp3", "49-44-33"); // MP3 audio
-        signatures.Add(".avi", "52-49-46-46"); // AVI video
+        signatures.Add(".mp4", "00-00-00-18-66-74-79-70"); // MPEG-4 video
+        signatures.Add(".mpeg", "00-00-01-BA"); // MPEG video
         signatures.Add(".msi", "D0-CF-11-E0-A1-B1-1A-E1"); // Windows Installer
+        signatures.Add(".ogg", "4F-67-67-53"); // Ogg Vorbis audio
+        signatures.Add(".pdf", "25-50-44-46-2D-31-2E"); // Adobe PDF
+        signatures.Add(".png", "89-50-4E-47"); // Portable Network Graphics
+        signatures.Add(".ppt", "D0-CF-11-E0"); // Microsoft PowerPoint (97-2003)
+        signatures.Add(".rtf", "7B-5C-72-74-66-31"); // Rich Text Format
+        signatures.Add(".sqlite", "53-51-4C-69-74-65-20-66"); // SQLite
+        signatures.Add(".swf", "43-57-53"); // Adobe Flash
+        signatures.Add(".wav", "52-49-46-46-57-41-56-45"); // Waveform Audio File Format (WAV)
+        signatures.Add(".webm", "1A-45-DF-A3-93-42-82-88-68-74-74-70-73-3A-2F-2F"); // WebM video
+        signatures.Add(".xls", "D0-CF-11-E0"); // Microsoft Excel (97-2003)
+        signatures.Add(".xml", "3C-3F-78-6D"); // XML document
+        signatures.Add(".xz", "FD-37-7A-58-5A-00"); // XZ archive
+        signatures.Add(".zip", "50-4B-03-04"); // ZIP archive
 
         // Get the directory path to traverse and output directory path from command-line arguments
         string dirPath = "";
